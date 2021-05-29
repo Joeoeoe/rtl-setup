d3202654d7d1184a0290956bb419964afee4b9a2
Joeoeoe <zhou_lihan@163.com>
2021-05-29 20:56:18 +0800

    第三步：解决框架配置问题——css module

--- test/example/Test.test.tsx
+++ test/example/Test.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Test from "./Test";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Test from './Test';
 
-describe("App", () => {
-  test("renders App component", () => {
+describe('App', () => {
+  test('renders App component', () => {
     render(<Test />);
   });
 });
